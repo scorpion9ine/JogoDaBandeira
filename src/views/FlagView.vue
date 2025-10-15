44a40ecd059fe4fb7bd0bc05dfe7c0e75174b4ba
scorpion9ine <mauricio.miguel34@gmail.com>
2025-10-14 21:21:10 -0300

    Atualização

--- src/views/FlagView.vue
+++ src/views/FlagView.vue
@@ -20,17 +20,96 @@ function verAcerto(id) {
 </script>
 
 <template>
-  <h2>{{ appStore.BC.name }}</h2>
-  <ul>
-    <li v-for="bandeiras in appStore.bandeiraSorteada" :key="bandeiras.name">
-      <img
-        :src="'bandeiras/' + bandeiras.image"
-        :alt="bandeiras.name"
-        @click="verAcerto(bandeiras.id)"
-      />
-      {{ bandeiras.name }}
-    </li>
-  </ul>
+  <div class="cronometro">
+    <h2>Cronômetro: {{ appStore.tempo }}</h2>
+  </div>
+  <div class="bandeiras">
+    <div class="nomePais">
+      <h2>{{ appStore.BC.name }}</h2>
+    </div>
+    <div class="bandeirasimg">
+      <ul>
+        <li v-for="bandeiras in appStore.bandeiraSorteada" :key="bandeiras.name">
+          <img
+            :src="'bandeiras/' + bandeiras.image"
+            :alt="bandeiras.name"
+            @click="verAcerto(bandeiras.id)"
+          />
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="pontuacao">
+    <h2>Pontuação: {{ appStore.pontos }}</h2>
+  </div>
 </template>
 
-<style></style>
+<style>
+.cronometro {
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
+  border: 5px solid #000000;
+  color: white;
+  text-align: start;
+  margin-bottom: 30px;
+
+h2 {
+  padding: 5px;
+  font-size: 2rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-style: bold;
+}
+}
+.bandeiras{
+
+  .nomePais {
+    font-size: 3rem;
+    width: 80%;
+    height: 90px;
+    background-color: #000000;
+    border: 5px solid #000000;
+    color: white;
+    text-align: center;
+    margin-bottom: 30px;
+    border-radius: 20px;
+    justify-content: center;
+    margin: 30px auto;
+    padding: 10px;
+  }
+div.bandeirasimg ul{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  border: 5px solid #000000;
+  margin: 10px;
+  background-color: black;
+  border-radius: 20px;
+}
+div.bandeirasimg ul li{
+  width: 30%;
+}
+}
+.pontuacao{
+  margin-top: 200px;
+  width: 80%;
+  height: 60px;
+  background-color: #000000;
+  border: 5px solid #000000;
+  color: white;
+  text-align: start;
+  margin: 30px auto;
+  border-radius: 20px;
+h2 {
+  padding: 5px;
+  font-size: 2rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-style: bold;
+}
+}
+body{
+ background-image: url('png/pngbandeira.png') ;
+
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,77 @@ import { RouterLink } from 'vue-router'
   <header>
     <h1>Que país é esse?</h1>
   </header>
-  <body>
-    <h2> COMO JOGAR? </h2>
-    <p>Selecione a bandeira que condiz com o nome do </p>
-  </body>
   <main>
-    <RouterLink to="flag-app">Iniciar</RouterLink>
+    <div class="gamebox">
+      <h2>COMO JOGAR?</h2>
+      <p>Selecione a bandeira que condiz com o nome do país!</p>
+    </div>
+    <div class="botao">
+      <RouterLink to="flag-app">INICIAR</RouterLink>
+    </div>
   </main>
 </template>
 
 <style>
-header{
-  padding: 20px;
+header {
+  margin: 50px;
+  padding: 0px 50px 0px 50px;
   text-align: center;
-  background-color: #4CAF50;
-  font-size: 4rem;
+  font-size: 3.5rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
+
+body {
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  background: #a39a9a;
+}
+
+.gamebox {
+  width: 300px;
+  height: 300px;
+  background-color: #000000;
+  border: 5px solid #000000;
+  margin: 50px auto;
+  color: white;
+  border-radius: 30px;
+
+  h2 {
+    padding: 10px 20px 10px 20px;
+    font-size: 2rem;
+    margin-top: 50px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-style: bold;
+  }
+
+  p {
+    padding: 0px 75px 0px 75px;
+    font-size: 1.5rem;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+}
+main {
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 3rem;
+  margin-top: 30%;
+
+.botao a{
+  background-color: #000000;
+  border: 5px solid #000000;
+  margin: 50px auto;
+  color: rgb(255, 255, 255);
+  border-radius: 30px;
+  padding: 17px 0px;
+  background-color: rgb(0, 0, 0);
+  display:inline-block;
+  width: 80%;
+}
+.botao{
+  margin-top: 150px;
+}
+}
+
 </style>
